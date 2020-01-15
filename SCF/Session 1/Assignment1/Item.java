b128aa3d12e6c5a17bf23fe5a6d05bd57ce8aec1
meta-nilesh-patel <59636579+meta-nilesh-patel@users.noreply.github.com>
2020-01-15 11:32:57 +0530

    Update Item.java

--- SCF/Session 1/Assignment1/Item.java
+++ SCF/Session 1/Assignment1/Item.java
@@ -5,33 +5,40 @@ public class Item {
 		private int quantity=0;
 		private double price;
 		
-		public Item(String name, double price){
+		public Item(String name, double price)
+		{
 			this.name = name;
 			this.price = price;
 		}
 		
-		public String getName(){
+		public String getName()
+		{
 			return this.name;
 		}
 		
 		
-		public int getQuantity(){
+		public int getQuantity()
+		{
 			return this.quantity;
 		}
 
-		public double getPrice(){
+		public double getPrice()
+		{
 			return this.price;
 		}
 		
-		public void setPrice(double price){
+		public void setPrice(double price)
+		{
 			this.price = price;
 		}
 		
-		public void incQuantity(int incQuantity){
+		public void incQuantity(int incQuantity)
+		{
 			this.quantity += incQuantity;
 		}
 		
-		public void decQuantity(int decQuantity){
+		public void decQuantity(int decQuantity)
+		{
 			this.quantity -= decQuantity;
 		}
 		
