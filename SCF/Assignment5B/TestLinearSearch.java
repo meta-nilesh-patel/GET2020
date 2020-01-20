5c322f390726f77da13daa16c0dde27e23421c71
meta-nilesh-patel <59636579+meta-nilesh-patel@users.noreply.github.com>
2020-01-20 22:56:55 +0530

    Update TestLinearSearch.java

--- SCF/Assignment5B/TestLinearSearch.java
+++ SCF/Assignment5B/TestLinearSearch.java
@@ -9,6 +9,7 @@ import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
 
 /**
+ *
  * @author Nilesh Patel
  * This class test linearSearch method of Searching class.
  */
