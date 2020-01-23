d9a5be58153eacc27cd3ef9f36aa825c2d71274c
meta-nilesh-patel <59636579+meta-nilesh-patel@users.noreply.github.com>
2020-01-23 15:28:43 +0530

    Update TestGetSizeMethod.java

--- SCF/Session 6/Assignment6A/TestGetSizeMethod.java
+++ SCF/Session 6/Assignment6A/TestGetSizeMethod.java
@@ -13,7 +13,6 @@ import org.junit.runners.Parameterized.Parameters;
  * @author Nilesh Patel
  * This class test getSize method of IntSet class;
  */
-
 @RunWith(Parameterized.class)
 public class TestGetSizeMethod
 {
@@ -37,9 +36,9 @@ public class TestGetSizeMethod
 	{ 
 		Object[][] data = new Object[][]
 			{
-				{new int[] {1,2,3,4,5}, 5},
+				{new int[] {1, 2, 3, 4, 5}, 5},
 				{new int[] {}, 0},
-				{new int[] {1,2,3,-1}, "Invalid Inputs! Input should in range of (1 to 1000)"}
+				{new int[] {1, 2, 3, -1}, "Invalid Inputs! Input should in range of (1 to 1000)"}
 			};
 		return Arrays.asList(data);		
 	}
