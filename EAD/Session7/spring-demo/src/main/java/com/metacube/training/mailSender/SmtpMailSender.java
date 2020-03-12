c049e99210809b0f4e76767bf6b82cc73b8dfe1b
meta-nilesh-patel <59636579+meta-nilesh-patel@users.noreply.github.com>
2020-03-12 11:32:20 +0530

    Update SmtpMailSender.java

--- EAD/Session7/spring-demo/src/main/java/com/metacube/training/mailSender/SmtpMailSender.java
+++ EAD/Session7/spring-demo/src/main/java/com/metacube/training/mailSender/SmtpMailSender.java
@@ -1,4 +1,6 @@
 package com.metacube.training.mailSender;
+
+import org.slf4j.Logger;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
@@ -10,5 +12,7 @@ public class SmtpMailSender implements MailSender {
 	public void sendMail()
 	{
 		System.out.println("MAIL SENT!!! SMTP sender");
+		final Logger log = org.slf4j.LoggerFactory.getLogger(SmtpMailSender.class);
+		log.info("Mail send from SMTP mail sender");
 	}
 }
