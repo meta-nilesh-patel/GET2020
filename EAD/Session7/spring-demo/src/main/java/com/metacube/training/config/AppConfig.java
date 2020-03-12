1362a92d578a02b4dab7e8ecf0c95954661eb736
meta-nilesh-patel <59636579+meta-nilesh-patel@users.noreply.github.com>
2020-03-12 11:20:04 +0530

    added comments.

--- EAD/Session7/spring-demo/src/main/java/com/metacube/training/config/AppConfig.java
+++ EAD/Session7/spring-demo/src/main/java/com/metacube/training/config/AppConfig.java
@@ -5,10 +5,12 @@ import org.springframework.context.annotation.Configuration;
 import com.metacube.training.mailSender.MockMailSender;
 import com.metacube.training.mailSender.SmtpMailSender;
 
+/**
+ * This method is creating bean objects of SmtpMailSender and MockMailSender class.
+ */
 @Configuration
 public class AppConfig 
 {
-
 	@Bean("mock")	
 	public MockMailSender mockMailSender() 
 	{
